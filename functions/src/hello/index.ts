701e6b7f54f6026ab5b439186269f299ab15743f
planethouki <26595148+planethouki@users.noreply.github.com>
2024-07-04 17:34:25 +0900

    add firebase function

---
+++ functions/src/hello/index.ts
@@ -0,0 +1,19 @@
+const functions = require('firebase-functions');
+const express = require('express');
+
+const app = express();
+const router = express.Router();
+
+// @ts-ignore
+router.get('/world', async (req, res, next) => {
+  res.send({
+    message: 'Hello, World!'
+  });
+});
+
+app.use('/', router);
+
+export const hello = functions
+  .runWith({ memory: '128MB',  timeoutSeconds: 2 })
+  .https
+  .onRequest(app);
