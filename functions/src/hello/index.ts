996e788ac687399e12e27f242b34ab2941a895d2
planethouki <26595148+planethouki@users.noreply.github.com>
2024-07-05 13:58:27 +0900

    finctions ip restrict

--- functions/src/hello/index.ts
+++ functions/src/hello/index.ts
@@ -5,6 +5,21 @@ const app = express();
 const router = express.Router();
 
 // @ts-ignore
+app.use((req, res, next) => {
+  const forwardedFor = req.headers['x-forwarded-for'];
+  if (!forwardedFor) {
+    res.status(403).send('Forbidden');
+    return;
+  }
+
+  if (forwardedFor.split(',').some(ip => ip.trim() === '34.54.110.179')) {
+    next();
+  } else {
+    res.status(403).send('Forbidden');
+  }
+});
+
+// @ts-ignore
 router.get('/world', async (req, res, next) => {
   console.log(req.headers);
   console.log(req.ip);
